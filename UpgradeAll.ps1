21fe4813413cc7b9cde9da5a6709a9518c3e0f98
michiel vanwelsenaere <vanwelsenaere.michiel@gmail.com>
2018-12-24 10:48:09 +0000

    Updated UpgradeAll.ps1

--- UpgradeAll.ps1
+++ UpgradeAll.ps1
@@ -1,6 +1,6 @@
 function CheckIfRunAsAdmin{
     $admin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
-    if($admin -eq $ false){
+    if($admin -eq $false){
         throw "script should be run as admin!"
     }
 }
