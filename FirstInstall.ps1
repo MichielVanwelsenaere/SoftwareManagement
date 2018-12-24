26ffed37b288003f67c1e00760da7e92e96c2002
michiel vanwelsenaere <vanwelsenaere.michiel@gmail.com>
2018-12-24 14:52:40 +0000

    Updated FirstInstall.ps1 with some minor cleanup tasks

--- FirstInstall.ps1
+++ FirstInstall.ps1
@@ -103,5 +103,14 @@ InstallAndConfigureOhMyPosh
 #Section for stuff that can't be scripted but shouldn't be forgotten
 # CodeSys3
 
+#Unpin as much as you can from start
+(New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items()| foreach { ($_).Verbs() | ?{$_.Name.Replace('&', '') -match 'From "Start" UnPin|Unpin from Start'} | %{$_.DoIt()}  }
+
+#Remove the desktop icons just made by the choco installations
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+Get-ChildItem -Path $DesktopPath -Include *.* -File -Recurse | foreach { $_.Delete()}
+$DesktopPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
+Get-ChildItem -Path $DesktopPath -Include *.* -File -Recurse | foreach { $_.Delete()}
+
 #set the global confirmation flag to disable
 choco feature disable -n allowGlobalConfirmation
