03f07023e80a657c3086d0d9878c8f0d768ad811
michiel vanwelsenaere <vanwelsenaere.michiel@gmail.com>
2018-12-24 10:45:35 +0000

    Updated UpgradeAll.ps1

--- UpgradeAll.ps1
+++ UpgradeAll.ps1
@@ -1,1 +1,9 @@
+function CheckIfRunAsAdmin{
+    $admin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+    if($admin -eq $ false){
+        throw "script should be run as admin!"
+    }
+}
+
+CheckIfRunAsAdmin
 choco upgrade all -y
