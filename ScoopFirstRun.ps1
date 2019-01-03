e0b9eb61cc15403f5911edd8a832c7b624358256
Michiel Vanwelsenaere <support@codit.eu>
2019-01-03 17:02:36 +0100

    changing to scoop approach

--- ScoopFirstRun.ps1
+++ ScoopFirstRun.ps1
@@ -1,21 +1,52 @@
 function InstallScoop{
     [environment]::setEnvironmentVariable('SCOOP','C:\Scoop','User')
-    $env:SCOOP='D:\Scoop'
+    $env:SCOOP='C:\Scoop'
     iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 }
 
-function CheckIfScoopInstalled {    
-    $ScoopInstalled = $false
-    if (Get-Command scoop.exe -ErrorAction SilentlyContinue) {
-        $ScoopInstalled = $true
-    }
-    return $ScoopInstalled
-}
 
-Write-Host "Checking if Scoop is installed"
-if(CheckIfScoopInstalled){
-    Write-Host "Scoop is already installed, skipping installation"
-}else{
-    Write-Host "Scoop installation not found, installing now.."
-    InstallScoop
-}
+#Will do nothing is Scoop is already installed
+InstallScoop
+
+#Install the extras backup
+scoop bucket add extras
+
+# Install essentials using Scoop
+scoop install cmder-full
+scoop install git
+scoop install oh-my-posh
+scoop install vscode
+# scoop install vscode-docker
+# scoop install vscode-gitlens
+# scoop install vscode-csharp
+# scoop install vscode-powershell
+# scoop install vscode-azurerm-tools
+# scoop install visualstudio2017enterprise
+scoop install dotnet-sdk
+scoop install docker
+scoop install docker-compose
+scoop install storageexplorer
+scoop install azure-ps
+scoop install servicebusexplorer
+scoop install azure-cli
+# scoop install winscp.install
+# scoop install sql-server-management-studio
+scoop install azure-functions-core-tools
+scoop install ilspy
+scoop install postman
+
+# Install nice to haves using Scoop
+scoop install lightshot
+scoop install keeweb
+scoop install telegram
+scoop install whatsapp
+
+# Installs software workonly
+# scoop install microsoft-teams
+scoop install slack
+
+# Software not downloadable with Scoop but that shouldn't be forgotten
+# plexmediaplayer
+# Spotify
+# GoogleChrome
+# office365proplus
